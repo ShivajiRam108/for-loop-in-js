a77377bf3d5a7719b4906a379b3b59bb6f86af5f
ShivajiRam108-github <ramudus684@gmail.com>
2024-12-15 15:22:41 +0530

    forloop

---
+++ script.js
@@ -0,0 +1,42 @@
+       
+console.log("...................Q -1...........")
+for (var a = 1 ; a<=20 ; a++){
+    console.log(a)
+}
+
+console.log("...................Q -2...........")
+        
+for (var r =1; r <= 10; r++){
+    console.log(r*r)
+        }
+console.log("...................Q -3...........")
+            
+for (var i =1; i <=10; i++)  {
+     console.log(i *5)
+}   
+            
+console.log("...................Q -4...........")
+for ( var a = 20; a >=1; a--){
+    console.log(a)
+}
+
+console.log("...................Q -5...........")
+
+for ( var a = 20; a>=0; a-=2){
+     console.log(a)
+}
+        
+console.log("...................Q -6...........")
+
+var array = [ 10, 20, 30 ,40 , 50];
+for (var i = 0; i < array.length; i++) {
+    console.log(array[i]);
+  }
+console.log("...................Q -7...........")           
+        
+
+ var arr = [`apple`, `banana`,`cherry`]
+for (var i = 0; i < arr.length; i++) {
+console.log(`Index: ${i},  ${arr[i]}`);
+}
+                             
